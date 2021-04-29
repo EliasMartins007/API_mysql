ba8f51d79acf293ae0b86ee7e1dcbf1f1db3b570
elias <eliasfaculdade@yahoo.com.br>
2021-04-29 12:48:16 -0300

    Post

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 APi simples de exmplo logrocket
 
-1. GET/programming-languages ->getMultiple()
-2. POST/programming-languages ->create()
-3. PUT/programming-languages/:id -> update()
-4. DELETE/programming-languages/:id -> remove()
+[x] GET/programming-languages ->getMultiple()
+[] POST/programming-languages ->create()
+[] PUT/programming-languages/:id -> update()
+[] DELETE/programming-languages/:id -> remove()
---
+++ config/winston.js
@@ -0,0 +1,23 @@
+// //var appRoute = require('app-root-path');// instalar ainda 29/04/2021 elias
+// var winston = require('winston');
+// var options = {
+//   file: {
+//     level: 'info',
+//     filename: '../logs/testApi.log', //`${}`//peaga caminho e log
+//     handleExceptions: true,
+//     json: true,
+//     maxsize: 5242880, //5MB
+//     maxFile: 5,
+//     colorize: false,
+//   },
+//   console: {
+//     level: 'debug',
+//     handleExceptions: true,
+//     json: false,
+//     colorize: true,
+//   },
+// };
+
+// var logger = new winston.Logger({
+//   transports: [],
+// });
